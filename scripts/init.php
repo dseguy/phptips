cdc880894211120a9143d21ad1bdeb48e4eb788b
Damien Seguy <dseguy@exakat.io>
2023-11-30 21:40:26 +0100

    new script to init tips from image

---
+++ scripts/init.php
@@ -0,0 +1,19 @@
+<?php
+
+$name = $argv[1];
+
+if (!file_exists('images/'.$name.'.png')) {
+	die("No such file as 'images/'.$name.'.png'.");
+}
+
+if (file_exists('docs/'.$name.'.png')) {
+	die("Already a file 'docs/$name.png'.");
+}
+
+$json = json_decode(file_get_contents('docs/skeleton.json'));
+$json->image = $name.'.png';
+$json->date = (new Datetime('now'))->format('Y-m-d');
+$json->title = $name;
+//print_r($json);
+
+file_put_contents('docs/'.$name.'.json', json_encode($json, JSON_PRETTY_PRINT));
