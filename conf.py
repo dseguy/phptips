e09dd0e73230baaf10b2170dac1d09b317652651
Damien Seguy <dseguy@exakat.io>
2023-12-03 17:18:39 +0100

    new tip about relaxed syntax; added google analytics verification

--- conf.py
+++ conf.py
@@ -53,3 +53,6 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Extra config to verify read the docs
+html_extra_path = ["googlee919cb0917e4fefc.html"]
